57c40d8124489e56df2a72513d73531e284f51fc
Aaron Pleitez <pleitez.aaron12@gmail.com>
2020-06-17 11:52:32 -0500

    implementing gql with images

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+const path = require(`path`);
+
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+  ],
+};
